f5d0d359993ead78ec70a1f1e125eee738e227f6
wanbiguizhao <shiweishiweihubugui@outlook.com>
2013-09-21 17:36:47 +0800

    QT

--- BalancedBinaryTree.cpp
+++ BalancedBinaryTree.cpp
@@ -4,6 +4,8 @@ private:
 	const int LH=1;
 	const int EH=0;
 	const int RH=-1;
+void LeftBalance(BSTree &pRoot);
+void RightBalance(BSTree &pRoot);
 public:
 	void R_Rotate(BSTree &pRoot);
 	void L_Rotate(BSTree &pRoot);
@@ -24,4 +26,51 @@ void AVLTree::L_Rotate(BSTree &pRoot)
 	BSTree rc=pRoot->pRChild;
 	pRoot->pRChild=rc->pLChild;
 	rc->pLChild=pRoot;
+}
+int InsertAVL(BSTree &pRoot,int key,bool &taller)
+{
+	if(!pRoot)
+	{
+		pRoot=new BSTNode();
+		pRoot->mData=key;
+		pRoot->pLChild=pRoot->pRChild=NULL;
+		taller=true;
+		return 1;
+	}
+	else
+	{
+		if(pRoot->mData==key)
+		{
+			taller=false;return 0;
+		}
+
+
+		if(pRoot->mData>key)
+		{
+			if(InsertAVL(pRoot->pLChild,key,taller)==0) return 0;
+			if(taller)
+			{
+				switch(pRoot->bf)
+				{
+					case LF: LeftBalance(pRoot);taller=false; break;
+					case EH: pRoot->bf=LH; taller=true; break;
+					case RH: pRoot->bf=EH; taller=false ;break;
+				}
+			}
+		}
+		else
+		{
+			if(InsertAVL(pRoot->pRChild,key,taller)==0) return 0;
+			if(taller)
+			{
+				switch(pRoot->bf)
+				{
+					case LF: pRoot->bf=EH;taller=false; break;
+					case EH: pRoot->bf=RH;taller=true;break;
+					case RF: 
+				}
+			}
+		}
+		return 1;
+	}
 }
